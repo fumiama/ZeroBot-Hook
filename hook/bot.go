f4797c49221567c350e21f1de2cf3636d85dfc11
fumiama <s862105088@gmail.com>
2021-10-14 00:00:44 +0800

    complete

--- hook/bot.go
+++ hook/bot.go
@@ -1,6 +1,8 @@
 package hook
 
 import (
+	"fmt"
+	"sync"
 	"unsafe"
 
 	zero "github.com/wdvxdr1123/ZeroBot"
@@ -26,11 +28,25 @@ type Driver interface {
 // BotConfig 运行中bot的配置，是Run函数的参数的拷贝
 var BotConfig *Config
 
+// 没有方法的interface
+type eface struct {
+	_type uintptr
+	data  unsafe.Pointer
+}
+
+func getdata(ptr *interface{}) unsafe.Pointer {
+	return (*eface)(unsafe.Pointer(ptr)).data
+}
+
 // Hook 改变本插件的环境变量以加载插件
-func Hook(botconf unsafe.Pointer, apicallers unsafe.Pointer, hooknew unsafe.Pointer) {
-	BotConfig = (*Config)(botconf)
-	APICallers = (*callerMap)(apicallers)
-	New = *(*(func() *Engine))(hooknew)
+func Hook(botconf interface{}, apicallers interface{}, hooknew interface{}, matlist interface{}, matlock interface{}) {
+	BotConfig = (*Config)(getdata(&botconf))
+	APICallers = (*callerMap)(getdata(&apicallers))
+	n := getdata(&hooknew)
+	New = *(*(func() *Engine))(unsafe.Pointer(&n))
+	matcherList = (*[]*Matcher)(getdata(&matlist))
+	matcherLock = (*sync.RWMutex)(getdata(&matlock))
+	fmt.Printf("[plugin]matlist: %p, matlock: %p\n", matcherList, matcherLock)
 }
 
 // GetBot 获取指定的bot (Ctx)实例
--- main.go
+++ main.go
@@ -10,35 +10,39 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-func Init(reg uintptr, del uintptr) {
-	go func() {
-		fmt.Println("enter init.")
-		ctrl.Register = *(*(func(service string, o *ctrl.Options) *zero.Engine))(unsafe.Pointer(reg))
-		ctrl.Delete = *(*(func(service string)))(unsafe.Pointer(del))
-		// -------------在此下书写插件内容-------------
-		fmt.Println("fill control success.")
-		en := ctrl.Register("demo", &ctrl.Options{
-			DisableOnDefault: false,
-			Help:             "help from demo",
+func Inita(reg interface{}, del interface{}) {
+	rd := getdata(&reg)
+	dd := getdata(&del)
+	ctrl.Register = *(*(func(service string, o *ctrl.Options) *zero.Engine))(unsafe.Pointer(&rd))
+	ctrl.Delete = *(*(func(service string)))(unsafe.Pointer(&dd))
+	// fmt.Printf("[plugin]set reg: %x, del: %x\n", ctrl.Register, ctrl.Delete)
+	// -------------在此下书写插件内容-------------
+	en := ctrl.Register("demo", &ctrl.Options{
+		DisableOnDefault: false,
+		Help:             "help from demo",
+	})
+	en.OnCommand("demo", zero.AdminPermission).SetBlock(true).SecondPriority().
+		Handle(func(ctx *zero.Ctx) {
+			fmt.Println("msg recv.")
+			ctx.SendChain(message.Text("回复"))
 		})
-		fmt.Println("register demo success.")
-		en.OnCommand("demo", zero.AdminPermission).SetBlock(true).SecondPriority().
-			Handle(func(ctx *zero.Ctx) {
-				fmt.Println("msg recv.")
-				ctx.SendChain(message.Text("回复"))
-			})
-		fmt.Println("register matcher success.")
-		// -------------在此上书写插件内容-------------
-		fmt.Println("quit init.")
-	}()
+	// -------------在此上书写插件内容-------------
 }
 
 // 以下勿动
 // Hook 改变本插件的环境变量以加载插件
-func Hook(botconf unsafe.Pointer, apicallers unsafe.Pointer, hooknew unsafe.Pointer) {
-	fmt.Println("enter hook.")
-	zero.Hook(botconf, apicallers, hooknew)
-	fmt.Println("quit hook.")
+func Hook(botconf interface{}, apicallers interface{}, hooknew interface{}, matlist interface{}, matlock interface{}) {
+	zero.Hook(botconf, apicallers, hooknew, matlist, matlock)
+}
+
+// 没有方法的interface
+type eface struct {
+	_type uintptr
+	data  unsafe.Pointer
+}
+
+func getdata(ptr *interface{}) unsafe.Pointer {
+	return (*eface)(unsafe.Pointer(ptr)).data
 }
 
 func main() {
